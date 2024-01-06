9843705b6f6b32f4159a4d3eb1655b1225140393
Shu12388y <spl.sp999@gmail.com>
2024-01-06 21:30:36 +0530

    file added

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/NavBar";
-import 'animate.css'
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,9 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div className="animate__animated animate__fadeInDown">
-          <Navbar />
-        </div>
+        <Navbar />
         {children}
       </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,26 @@
-import Footer from '@/components/Footer'
-import Sider from '@/components/Sider'
-import HeroSection from '@/components/HeroSection'
-import NewsLetter from '@/components/NewsLetter'
-import Feature from '@/components/Feature'
-import Faq from '@/components/FAQ'
-import Cta from '@/components/CTA'
-import SectionTestional from '@/components/SectionTestional'
-import 'animate.css'
+import Footer from "@/components/Footer";
+import Sider from "@/components/Sider";
+import HeroSection from "@/components/HeroSection";
+import NewsLetter from "@/components/NewsLetter";
+import Feature from "@/components/Feature";
+import Faq from "@/components/FAQ";
+import Cta from "@/components/CTA";
+import SectionTestional from "@/components/SectionTestional";
+import "animate.css";
 export default function Home() {
   return (
     <>
-    
-      <HeroSection/>
-      <Sider/>
-      <SectionTestional/>
-      <Faq/>
-      
+      <HeroSection />
+      <Sider />
+      <SectionTestional />
+      <Faq />
 
-      <Feature/>
-      <Cta/>
+      <Feature />
+      <Cta />
 
-      <NewsLetter/>
-      
-      <Footer/>
+      <NewsLetter />
+
+      <Footer />
     </>
-  )
+  );
 }
