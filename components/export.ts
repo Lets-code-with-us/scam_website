256a4eee773109da6aa69d2fe6b6f2f422a1558b
Shu12388y <spl.sp999@gmail.com>
2024-01-07 15:36:16 +0530

    export file added

---
+++ components/export.ts
@@ -0,0 +1,31 @@
+import Navbar from "./NavBar";
+import Avatar from "./Avator";
+import Card from "./Card";
+import Faq from "./FAQ";
+import Feature from "./Feature";
+import Footer from "./Footer";
+import HeroSection from "./HeroSection";
+import NewsLetter from "./NewsLetter";
+import SidebarOne from "./SideBar";
+import SectionTestional from "./SectionTestional";
+import TestimonialOne from "./Testinomal";
+import TrendingCard from "./TrendingCard";
+
+
+
+
+
+export default{
+    Navbar,
+    Avatar,
+    Faq,
+    TestimonialOne,
+    TrendingCard,
+    SectionTestional,
+    SidebarOne,
+    HeroSection,
+    Footer,
+    Card,
+    NewsLetter,
+    Feature
+}
