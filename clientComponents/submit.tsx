b64a2db499c107381f51756c683824ab2bb376d0
Shu12388y <spl.sp999@gmail.com>
2024-01-08 10:19:55 +0530

    client component added

---
+++ clientComponents/submit.tsx
@@ -0,0 +1,15 @@
+"use client";
+function SubmitButton() {
+  return (
+    <button
+      type="button"
+      className="w-full rounded-md bg-black px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
+    >
+      Subscribe
+    </button>
+  );
+}
+
+
+
+export default SubmitButton
